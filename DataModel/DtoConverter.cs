6b8e8a9ae279201b31d5e0306ed70eace92e7b43
stalli <stalli20@gmail.com>
2019-08-14 10:04:56 +0300

    DtoConverter

---
+++ DataModel/DtoConverter.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using DataModel.Dtos;
+
+namespace DataModel
+{
+    public static class DtoConverter
+    {
+        public static IEnumerable<ColumnDto> ToDto(this IEnumerable<Column> columns, IEnumerable<Card> cards)
+        {
+            return columns.Select(col => new ColumnDto
+            {
+                Id = col.Id,
+                OrderNo = col.OrderNo,
+                Title = col.Title,
+                Cards = cards.Where(car => car.ColumnId == col.Id)
+            });
+        }
+
+        public static Column ToEntity(this ColumnDto dto)
+        {
+            return new Column
+            {
+                Id = dto.Id,
+                OrderNo = dto.OrderNo,
+                Title = dto.Title,
+                UserId = dto.UserId
+            };
+        }
+    }
+}
